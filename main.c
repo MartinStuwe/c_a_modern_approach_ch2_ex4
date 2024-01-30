733245a783f96fb1219ae3446528c08b9b321885
Martin Stuwe <MartinStuwe@ProtonMail.com>
2024-01-30 18:54:10 +0100

    add main.c main

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+    int thisisanint;
+    float thisisafloat;
+    printf("thisisanint: %d\n", thisisanint);
+    printf("thisisafloat: %d\n", thisisafloat);
+    return 0;
+}
